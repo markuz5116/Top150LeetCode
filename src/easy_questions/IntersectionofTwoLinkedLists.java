981f7f4f201f0553164bc5f53ce1740ea6790313
Marcus Ong <markuz5116@gmail.com>
2023-06-03 17:32:28 +0800

    Add solution with O(M+N) for time and space

---
+++ src/easy_questions/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,35 @@
+package easy_questions;
+
+import java.util.HashSet;
+
+public class IntersectionofTwoLinkedLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    // Given the heads of two singly linked-lists headA and headB, return the node
+    // at which the two lists intersect. If the two linked lists have no
+    // intersection at all, return null.
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> visited = new HashSet<>();
+        while (headA != null) {
+            visited.add(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            if (visited.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        
+        return null;
+    }
+}

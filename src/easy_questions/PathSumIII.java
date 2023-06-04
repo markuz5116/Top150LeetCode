a6ae7e2f5e24f7caee03ab949b1cc88b80dd3cb6
Marcus Ong <markuz5116@gmail.com>
2023-06-04 15:30:35 +0800

    Add solution for path sum III

--- src/easy_questions/PathSumIII.java
+++ src/easy_questions/PathSumIII.java
@@ -1,5 +1,8 @@
 package easy_questions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PathSumIII {
     public class TreeNode {
         int val;
@@ -20,19 +23,38 @@ public class PathSumIII {
         }
     }
 
-    public int pathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return 0;
+    class CountWrapper {
+        int count;
+
+        CountWrapper(int count) {
+            this.count = count;
         }
-        return pathSum(root.left, targetSum) + pathSum(root.right, targetSum) + helper(root, targetSum + 0.0);
     }
 
-    public int helper(TreeNode root, Double targetSum) {
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> sumToCount = new HashMap<>();
+        CountWrapper countWrapper = new CountWrapper(0);
+        helper(root, 0, targetSum, countWrapper, sumToCount);
+        return countWrapper.count;
+    }
+
+    private void helper(TreeNode root, long currSum, int targetSum, CountWrapper countWrapper, Map<Long, Integer> sumToCount) {
         if (root == null) {
-            return 0;
+            return;
         }
-        int hit = targetSum - root.val == 0 ? 1 : 0;
-        return hit + helper(root.left, targetSum - root.val) + helper(root.right, targetSum - root.val);
+
+        currSum += root.val;
+        if (currSum == targetSum) {
+            countWrapper.count++;
+        }
+        countWrapper.count += sumToCount.getOrDefault(currSum - targetSum, 0);
+
+        sumToCount.put(currSum, sumToCount.getOrDefault(currSum, 0) + 1);
+
+        helper(root.left, currSum, targetSum, countWrapper, sumToCount);
+        helper(root.right, currSum, targetSum, countWrapper, sumToCount);
+
+        sumToCount.put(currSum, sumToCount.getOrDefault(currSum, 0) - 1);
     }
 
 }

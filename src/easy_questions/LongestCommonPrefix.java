d719f31a568d7f9bb801d53ad2606e40701c4b75
Marcus Ong <markuz5116@gmail.com>
2023-06-06 12:08:57 +0800

    Use Trie instead

--- src/easy_questions/LongestCommonPrefix.java
+++ src/easy_questions/LongestCommonPrefix.java
@@ -1,37 +1,45 @@
 package easy_questions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestCommonPrefix {
+    public class TrieNode {
+        Map<Character, TrieNode> children;
+        int count;
+
+        public TrieNode() {
+            this.children = new HashMap<>();
+            this.count = 0;
+        }
+    }
+
     public String longestCommonPrefix(String[] strs) {
-        int longestPrefix = -1;
-        while (longestPrefix < strs[0].length()) {
-            longestPrefix++;
-            char commonChar = 0;
-            boolean hasMiss = false;
-            for (String str : strs) {
-                if (str.length() <= longestPrefix) {
-                    hasMiss = true;
-                    break;
-                }
+        TrieNode root = new TrieNode();
+        int n = strs.length;
+        root.count = n;
 
-                if (commonChar == 0) {
-                    commonChar = str.charAt(longestPrefix);
-                    continue;
+        for (String str : strs) {
+            TrieNode curr = root;
+            for (int i = 0; i < str.length(); i++) {
+                if (!curr.children.containsKey(str.charAt(i))) {
+                    curr.children.put(str.charAt(i), new TrieNode());
                 }
-
-                if (str.charAt(longestPrefix) != commonChar) {
-                    hasMiss = true;
-                    break;
-                }
-            }
-            if (hasMiss) {
-                break;
+                curr = curr.children.get(str.charAt(i));
+                curr.count++;
             }
         }
 
-        if (longestPrefix == -1) {
-            return "";
+        String longestPrefix = "";
+        while (root.count == n && !root.children.isEmpty()) {
+            for (Map.Entry<Character, TrieNode> entry : root.children.entrySet()) {
+                Character c = entry.getKey();
+                root = entry.getValue();
+                if (root.count == n) {
+                    longestPrefix += c;
+                }
+            }
         }
-
-        return strs[0].substring(0, longestPrefix);
+        return longestPrefix;
     }
 }
